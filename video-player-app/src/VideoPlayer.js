9208e5185c17716e0ce5694d0ab3b28b82687bb4
Kenneth Trott <kentrottjr@gmail.com>
2020-10-03 14:46:22 -0700

    Refactor VideoPlayer to stateful component.

--- video-player-app/src/VideoPlayer.js
+++ video-player-app/src/VideoPlayer.js
@@ -7,16 +7,32 @@ import VideoList from './VideoList.js'
 class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            videos: videosList,
+            selectedVideoIndex: 0,
+        }
+    }
+
+    componentDidMount() {
+        //update state with id
+        this.setState({
+            id: getVideoIdFromPageUrl(this.state.videos[this.state.selectedVideoIndex].url)
+        })
     }
 
     render() {
-        var videos = videosList;
-        var selectedVideoIndex = 0;
+        // var videos = videosList;
+        // var selectedVideoIndex = 0;
 
-        var id = getVideoIdFromPageUrl(videos[selectedVideoIndex].url);
-        console.log(videos);
+        // var id = getVideoIdFromPageUrl(videos[selectedVideoIndex].url);
+        // console.log(videos);
+
+        var videos = this.state.videos;
+        var selectedVideoIndex = this.state.selectedVideoIndex;
+        var id = this.state.id;
 
         var rootClassName = 'container-section';
+        
 
         return (
             <div className={rootClassName}>
