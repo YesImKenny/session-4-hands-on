d4e131696f5fddd5e9ffebd2414ab83e1341ccd5
Kenneth Trott <kentrottjr@gmail.com>
2020-10-04 13:51:09 -0700

    Refactor VideoButton component from Class to Functional.

--- video-player-app/src/VideoButton.js
+++ video-player-app/src/VideoButton.js
@@ -1,37 +1,31 @@
 import * as React from 'react';
 
-class VideoButton extends React.Component {
+const VideoButton = (props) => {
+  const {
+    videoTitle,
+    videoImage,
+    videoDescription,
+    isSelected,
+    changeVideo,
+  } = props;
 
-    render() {
-      // Get the required props to be used in this component
-        var {
-            videoTitle,
-            videoImage,
-            videoDescription,
-            isSelected,
-            changeVideo,
-        } = this.props;
+  let buttonClassName = 'video';
 
-        var buttonClassName = 'video';
+  if (isSelected) {
+    buttonClassName = 'video selected';
+  }
 
-        if (isSelected) {
-            buttonClassName = 'video selected';
-        }
-        
-      // Make sure you pass the right prop values in the blanks provided
-
-        return (
-            <div
-                className={buttonClassName} onClick={changeVideo}
-            >
-                <img src={`${videoImage}`} alt="" className="video-image" />
-                    <div className="description">
-                        <h4>{videoTitle}</h4>
-                        <p>{videoDescription}</p>
-                    </div>
-            </div>
-        );
-    }
+  return (
+    <div
+      className={buttonClassName} onClick={changeVideo}
+    >
+      <img src={`${videoImage}`} alt="" className="video-image" />
+        <div className="description">
+          <h4>{videoTitle}</h4>
+          <p>{videoDescription}</p>
+        </div>
+    </div>
+  );
 }
 
 export default VideoButton;
