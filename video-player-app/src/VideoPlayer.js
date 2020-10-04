058570eeb854536daf1e592853fc5d1d31b01120
Kenneth Trott <kentrottjr@gmail.com>
2020-10-04 13:39:39 -0700

    Refactor VideoPlayer component using Hooks.

--- video-player-app/src/VideoPlayer.js
+++ video-player-app/src/VideoPlayer.js
@@ -4,60 +4,44 @@ import { videosList } from "./constants";
 import VideoList from './VideoList.js'
 
 
-class VideoPlayer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            videos: videosList,
-            selectedVideoIndex: 0,
-        }
-    }
+const VideoPlayer = () => {
+  const [videos, setVideos] = React.useState(videosList);
+  const [selectedVideoIndex, setSelectedVideoIndex] = React.useState(0);
+  const [id, setId] = React.useState();
 
-    componentDidMount = () => {
-      this.setState({
-        id: getVideoIdFromPageUrl(this.state.videos[this.state.selectedVideoIndex].url)
-      })
-    }
+  React.useEffect(() => {
+    setId(getVideoIdFromPageUrl(videos[selectedVideoIndex].url));
+  });
 
 
-    changeVideo = (idx) => {
-      this.setState((state, props) => ({
-        selectedVideoIndex: idx,
-        id: getVideoIdFromPageUrl(state.videos[idx].url)
-      }))
-    }
-      
+  const changeVideo = (idx) => {
+    setSelectedVideoIndex(idx);
+    setId(getVideoIdFromPageUrl(videos[idx].ur));
+  }
+  
+  let rootClassName = 'container-section';
 
-    render() {
-        var videos = this.state.videos;
-        var selectedVideoIndex = this.state.selectedVideoIndex;
-        var id = this.state.id;
-
-        var rootClassName = 'container-section';
-        
-
-        return (
-            <div className={rootClassName}>
-                <div className="main-section">
-                    <iframe
-                        className="video-player"
-                        title="Video Player"
-                        src={`https://player.vimeo.com/video/${id}?portrait=0`}
-                        width="1000"
-                        height="348"
-                        frameBorder="0"
-                        allow="autoplay; fullscreen"
-                        allowFullScreen
-                    />
-                </div>
-                <VideoList
-                        videos={videos}
-                        selectedVideoIndex={selectedVideoIndex}
-                        changeVideo={this.changeVideo}
-                />
-            </div>
-        );
-    }
+  return (
+    <div className={rootClassName}>
+        <div className="main-section">
+            <iframe
+                className="video-player"
+                title="Video Player"
+                src={`https://player.vimeo.com/video/${id}?portrait=0`}
+                width="1000"
+                height="348"
+                frameBorder="0"
+                allow="autoplay; fullscreen"
+                allowFullScreen
+            />
+        </div>
+        <VideoList
+                videos={videos}
+                selectedVideoIndex={selectedVideoIndex}
+                changeVideo={changeVideo}
+        />
+    </div>
+  );
 }
 
 export default VideoPlayer;
